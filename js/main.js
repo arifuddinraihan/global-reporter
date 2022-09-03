714771d6500674ef2b1f16b1e519f4ac2dbee2bd
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-09-03 17:46:16 +0600

    shorting system added

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ const catagoryData = async () => {
     return data.data.news_category;
 };
 
+// catagory names inside buttons
 const setCatagory = async () => {
 
     // start spinner
@@ -31,6 +32,7 @@ const setCatagory = async () => {
     loadingSpinner(false);
 }
 
+// News details inside cards
 const catagoryNews = async (id) => {
     // start spinner
     loadingSpinner(true);
@@ -41,9 +43,6 @@ const catagoryNews = async (id) => {
     const response = await fetch(url);
     const data = await response.json();
     const everyNews = data.data;
-
-    // everyNews.sort();
-    // console.log(everyNews);
 
     const newsNumbers = document.getElementById("news-numbers")
     newsNumbers.innerText = "";
@@ -116,13 +115,132 @@ const catagoryNews = async (id) => {
 
     });
 
+    document.getElementById("hight-to-low").addEventListener('click', () => {
+        everyNews.sort(function (x, y) {
+            return y.total_view - x.total_view;
+        });
+        const newsContainer = document.getElementById("news-container");
+        newsContainer.innerHTML = "";
+        everyNews.forEach(news => {
+            // console.log(news);
+            const div = document.createElement("div");
+            div.classList.add("row");
+            div.classList.add("my-2");
+            div.classList.add("shadow-lg");
+            div.classList.add("align-items-center");
+            div.classList.add("rounded-3");
+            //    div.classList.add("g-0");
+            div.innerHTML = `
+                            <div class="col-md-2 py-2">
+                                <img src=${news.thumbnail_url} class="img-fluid" alt="...">
+                            </div>
+                            <div class="col-md-10">
+                                <div class="card-body">
+                                    <h5 class="card-title">${news.title}</h5>
+                                    <p class="card-text text-secondary">${news.details.slice(0, 250) ? news.details.slice(0, 250) + "..." : "No Details Shared"}</p>
+                                    <p class="card-text">
+                                        <small class="text-muted">
+                                            <div class="container text-start">
+                                                <div class="row row-cols-sm-1 align-items-center">
+                                                    <div class="col-md-4 col-xs-12">
+                                                        <div class="row align-items-center">
+                                                            <div class="col-3">
+                                                            <img src=${news.author.img} class="img-thumbnail rounded-circle float-start" alt="...">
+                                                            </div>
+                                                            <div class="col-9">
+                                                            ${news.author.name ? news.author.name : "No Name found"}
+                                                            <br/>
+                                                            ${news.author.published_date ? news.author.published_date : "No date found"}
+                                                            </div>
+                                                        </div>
+                                                        </div>
+                                                        <div class="col-md-4 col-xs-12" style="margin-top: 20px;">
+                                                          <i class="fa-solid fa-eye p-2"></i>
+                                                          <span class="p-2">${news.total_view ? news.total_view : "N / A"}</span>
+                                                        </div>
+                                                        <div class="col-md-4 col-xs-12" style="margin-top: 20px;">
+                                                            <button onclick="modalNews('${news._id}')" type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Read More...</button>
+                                                        </div>
+                                                </div>
+                                            </div>
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+           
+           ` ;
+            newsContainer.appendChild(div);
+
+        });
+
+    });
+
+    document.getElementById("low-to-high").addEventListener('click', () => {
+        everyNews.sort(function (x, y) {
+            return x.total_view - y.total_view;
+        });
+        const newsContainer = document.getElementById("news-container");
+        newsContainer.innerHTML = "";
+        everyNews.forEach(news => {
+            // console.log(news);
+            const div = document.createElement("div");
+            div.classList.add("row");
+            div.classList.add("my-2");
+            div.classList.add("shadow-lg");
+            div.classList.add("align-items-center");
+            div.classList.add("rounded-3");
+            //    div.classList.add("g-0");
+            div.innerHTML = `
+                            <div class="col-md-2 py-2">
+                                <img src=${news.thumbnail_url} class="img-fluid" alt="...">
+                            </div>
+                            <div class="col-md-10">
+                                <div class="card-body">
+                                    <h5 class="card-title">${news.title}</h5>
+                                    <p class="card-text text-secondary">${news.details.slice(0, 250) ? news.details.slice(0, 250) + "..." : "No Details Shared"}</p>
+                                    <p class="card-text">
+                                        <small class="text-muted">
+                                            <div class="container text-start">
+                                                <div class="row row-cols-sm-1 align-items-center">
+                                                    <div class="col-md-4 col-xs-12">
+                                                        <div class="row align-items-center">
+                                                            <div class="col-3">
+                                                            <img src=${news.author.img} class="img-thumbnail rounded-circle float-start" alt="...">
+                                                            </div>
+                                                            <div class="col-9">
+                                                            ${news.author.name ? news.author.name : "No Name found"}
+                                                            <br/>
+                                                            ${news.author.published_date ? news.author.published_date : "No date found"}
+                                                            </div>
+                                                        </div>
+                                                        </div>
+                                                        <div class="col-md-4 col-xs-12" style="margin-top: 20px;">
+                                                          <i class="fa-solid fa-eye p-2"></i>
+                                                          <span class="p-2">${news.total_view ? news.total_view : "N / A"}</span>
+                                                        </div>
+                                                        <div class="col-md-4 col-xs-12" style="margin-top: 20px;">
+                                                            <button onclick="modalNews('${news._id}')" type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Read More...</button>
+                                                        </div>
+                                                </div>
+                                            </div>
+                                        </small>
+                                    </p>
+                                </div>
+                            </div>
+           
+           ` ;
+            newsContainer.appendChild(div);
+
+        });
+
+    });
+
     // stop spinner
     loadingSpinner(false);
+
 };
 
-
-
-
+// Modal News Details
 const modalNews = async (newsId) => {
     // start spinner
     loadingSpinner(true);
